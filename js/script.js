df8b920969008c7102462107cb428cbf42fb6e9a
AehmreHD <emyenschtcool0@gmail.com>
2026-09-03 15:17:27 +0200

    Refactor menu button event listener code

--- js/script.js
+++ js/script.js
@@ -4,11 +4,3 @@ const topbarLinks = document.getElementById("topbarLinks");
 menuButton.addEventListener("click", function() {
     topbarLinks.classList.toggle("open");
 });
-
-const links = topbarLinks.querySelectorAll("a");
-
-links.forEach(function(link) {
-    link.addEventListener("click", function() {
-        topbarLinks.classList.remove("open");
-    });
-});
